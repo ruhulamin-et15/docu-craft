f5f2d714d38ce00e7281e9a551e43c4a2df6ffe5
ruhulamin-et15 <ruhulamin.et15@gmail.com>
2024-03-23 06:32:30 +0600

    update

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { getDocuments } from "@/lib/doc";
 import { Inter } from "next/font/google";
@@ -18,7 +19,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="h-full lg:ml-72 xl:ml-80">
           <Header docs={allDocuments} />
-          <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
+          <div className="relative px-4 pt-14 sm:px-6 lg:px-8 min-h-[90vh]">
             <main className="flex-auto py-16">
               <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
                 <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
               {children}
             </main>
           </div>
+          <Footer />
         </div>
       </body>
     </html>
---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="mx-auto max-w-2xl space-y-10 pb-5 lg:max-w-5xl">
+      <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
+        <p className="text-xs text-zinc-600 dark:text-zinc-400">
+          © Copyright 2023. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
