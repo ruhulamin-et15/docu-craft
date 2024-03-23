783bcb7eaf49d185f1bfd1dad3d2420e9215dbb4
ruhulamin-et15 <ruhulamin.et15@gmail.com>
2024-03-23 06:22:52 +0600

    update

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 const Landing = () => {
   return (
@@ -13,7 +13,7 @@ const Landing = () => {
               </h1>
 
               <p className="mt-6 text-base leading-7 text-slate-700">
-                It doesn't matter if you have an API if nobody knows how to use
+                It doesnt matter if you have an API if nobody knows how to use
                 it. Teach people the ins and outs of OAuth 2.0 and JWTs in style
                 with Protocol, a beautiful API documentation template.
               </p>
@@ -21,7 +21,8 @@ const Landing = () => {
               <div className="mt-10 flex flex-wrap gap-4">
                 <Link
                   href="/docs/introduction"
-                  className="inline-flex justify-center rounded-lg bg-slate-900 px-4 py-3 text-sm font-semibold text-white hover:bg-slate-700">
+                  className="inline-flex justify-center rounded-lg bg-slate-900 px-4 py-3 text-sm font-semibold text-white hover:bg-slate-700"
+                >
                   <span>Get Started</span>
                 </Link>
               </div>
@@ -30,6 +31,7 @@ const Landing = () => {
               <Image
                 src="/banner.png"
                 width="1600"
+                alt="banner"
                 height="1280"
                 className="relative z-20 -mb-36 aspect-[853/682] max-w-[630px] rounded-xl bg-slate-200 shadow-xl shadow-black/5 ring-1 ring-slate-900/5 sm:-mb-16 lg:-mb-8 xl:-mb-16"
               />
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -54,7 +54,7 @@ const Sidebar = ({ docs }) => {
 
     setRootNodes([...roots]);
     setNonRootNodesGrouped({ ...nonRoots });
-  }, [pathName]);
+  }, [pathName, docs]);
 
   return (
     <nav className="lg:block my-10">
